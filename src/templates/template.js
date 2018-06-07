fb7a032e0ca6336e6eec7e55c1d0db5a1dc2dc18
ByronGBP <byrongbp@gmail.com>
2018-06-07 17:56:54 +0200

    add object with templates

---
+++ src/templates/template.js
@@ -0,0 +1,22 @@
+export const Template = {
+  emailPassword: `
+    <div id="title" class="space">
+      <span class="title-text">Sign up</span>
+    </div>
+    <div id="inputs-field" class="flex space">
+      <div class="space flex">
+        <input type="email" class="input-field">
+        <label class="placeholder">Email</label>
+        <span class="border-focus"></span>
+      </div>
+      <div class="space flex">
+        <input type="text" class="input-field">
+        <label class="placeholder">Password</label>
+        <span class="border-focus"></span>
+      </div>
+      <div class="space flex">
+        <button class="next">Next</button>
+      </div>
+    </div>
+    `
+};

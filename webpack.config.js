b057914756fc8935606e3c92ad3c93419f68e4b8
ByronGBP <byrongbp@gmail.com>
2018-06-07 16:34:34 +0200

    add css support

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/app.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new CopyWebpackPlugin([
+      { from: 'src/styles', to: 'styles' }
+    ])
+  ],
+  module: {
+    rules: [{
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  }
+};

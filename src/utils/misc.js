002d7f66e51b249e7a38c6ed565d959d6dba7f0d
ByronGBP <byrongbp@gmail.com>
2018-06-09 21:21:03 +0200

    add string comparations

--- src/utils/misc.js
+++ src/utils/misc.js
@@ -1,3 +1,38 @@
+/**
+ * Little library with miscellaneous operations
+ */
+
 export function parseToArray (item) {
   return item.length && typeof item === 'object' ? item : [item];
 };
+
+export function parseToObject (arr, keys) {
+  let object = {};
+  if (arr.length !== keys.length) {
+    throw new Error('Array and keys must have same length', arr, keys);
+  }
+  for (let i = 0; i < arr.length; i++) {
+    object[keys[i]] = arr[i];
+  }
+
+  return object;
+}
+
+export function hasUpperCase (str) {
+  let regex = /[A-Z]/;
+  return regex.test(str);
+}
+
+export function hasSymbol (str) {
+  let regex = /\W/;
+  return regex.test(str);
+}
+
+export function hasNumber (str) {
+  let regex = /[0-9]/;
+  return regex.test(str);
+}
+
+export function hasLength (str, length) {
+  return str.length >= length;
+}
